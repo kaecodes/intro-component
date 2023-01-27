678d818b6ee3e0784f8dd1837a6a120cdb5e262f
Kaela Nguyen <kaedtn96@gmail.com>
2023-01-26 16:57:00 -0800

    add functionality to display and remove error icons

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ const firstName = document.getElementById('fname');
 const lastName = document.getElementById('lname'); 
 const email = document.getElementById('email'); 
 const password = document.getElementById('password'); 
-const errorIcon = document.querySelectorAll('error-icon'); 
 const claimBtn = document.getElementById('free-trial'); 
 
 document.forms['myForm'].onsubmit = function(e) {
@@ -12,6 +11,9 @@ document.forms['myForm'].onsubmit = function(e) {
   if (firstName.value == '') {
     document.getElementById('fname-error').innerHTML = 'First Name cannot be empty'; 
     document.getElementById('fname-error').style.display = 'block'; 
+    document.getElementById('fname-error-icon').style.display = 'block'; 
+    document.getElementById('fname').style.borderColor = 'var(--red)'; 
+    document.getElementsByName('fname')[0].placeholder = ''; 
     e.preventDefault(); 
     submit = false; 
   }
@@ -19,6 +21,9 @@ document.forms['myForm'].onsubmit = function(e) {
   if (lastName.value == '') {
     document.getElementById('lname-error').innerHTML = 'Last Name cannot be empty'; 
     document.getElementById('lname-error').style.display = 'block'; 
+    document.getElementById('lname-error-icon').style.display = 'block'; 
+    document.getElementById('lname').style.borderColor = 'var(--red)'; 
+    document.getElementsByName('lname')[0].placeholder = ''; 
     e.preventDefault(); 
     submit = false; 
   }
@@ -26,9 +31,15 @@ document.forms['myForm'].onsubmit = function(e) {
   if (email.value == '') {
     document.getElementById('email-error').innerHTML = 'Email cannot be empty'; 
     document.getElementById('email-error').style.display = 'block'; 
+    document.getElementById('email-error-icon').style.display = 'block'; 
+    document.getElementById('email').style.borderColor = 'var(--red)'; 
+    document.getElementsByName('email')[0].placeholder = ''; 
   } else if (!email.value.match(emailFormat)) {
     document.getElementById('email-error').innerHTML = 'Looks like this is not an email'; 
     document.getElementById('email-error').style.display = 'block'; 
+    document.getElementById('email-error-icon').style.display = 'block'; 
+    document.getElementById('email').style.borderColor = 'var(--red)'; 
+    email.classList.add('error'); 
     e.preventDefault(); 
     submit = false; 
   }
@@ -36,6 +47,9 @@ document.forms['myForm'].onsubmit = function(e) {
   if (password.value == '') {
     document.getElementById('password-error').innerHTML = 'Password cannot be empty'; 
     document.getElementById('password-error').style.display = 'block'; 
+    document.getElementById('password-error-icon').style.display = 'block'; 
+    document.getElementById('password').style.borderColor = 'var(--red)'; 
+    document.getElementsByName('password')[0].placeholder = ''; 
     e.preventDefault(); 
     submit = false; 
   }
@@ -45,6 +59,7 @@ document.forms['myForm'].onsubmit = function(e) {
 
 function removeError() {
   document.getElementById(this.id + '-error').innerHTML = ''; 
+  document.getElementById(this.id + '-error-icon').style.display = 'none'; 
 }
 
 document.getElementById('fname').onkeyup = removeError; 
